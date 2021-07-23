bc740d473335acda2d36d6324270ed604252e4fb
Xazeq <setezey@gmail.com>
2021-07-23 09:16:24 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,38 +4,54 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.Post;
-import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.grabber.utils.DateTimeParser;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        int page = 1;
-        while (page <= 5) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page++).get();
-            Elements row = doc.select(".postslisttopic");
-            Elements date = doc.select(".altCol");
-            int index = 1;
-            for (Element td : row) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(date.get(index).text());
-                index += 2;
-            }
-        }
+public class SqlRuParse implements Parse {
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
-    public static Post getPost(String url) throws Exception {
-        Document doc = Jsoup.connect(url).get();
-        Elements header = doc.select(".messageHeader");
-        Elements body = doc.select(".msgBody");
-        Elements footer = doc.select(".msgFooter");
-        String title = header.get(0).text()
-                .split(" \\[new]")[0];
-        String desc = body.get(1).text();
-        LocalDateTime created = new SqlRuDateTimeParser()
-                .parse(footer.first().text().split(" \\[")[0]);
-        return new Post(title, url, desc, created);
+    @Override
+    public List<Post> list(String link) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                posts.add(detail(href.attr("href")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String link) {
+        String title = "default";
+        String desc = "default";
+        LocalDateTime created = LocalDateTime.now();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements header = doc.select(".messageHeader");
+            Elements body = doc.select(".msgBody");
+            Elements footer = doc.select(".msgFooter");
+            title = header.get(0).text()
+                    .split(" \\[new]")[0];
+            desc = body.get(1).text();
+            created = dateTimeParser.parse(footer.first().text().split(" \\[")[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Post(title, link, desc, created);
     }
 }
